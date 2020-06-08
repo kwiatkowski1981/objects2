e3fb5968d69feb58f4f1f83a343bf5e0e337f968
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2020-06-08 22:29:20 +0200

    feat: added a new classes, created 3 new objects

---
+++ src/Company.java
@@ -0,0 +1,23 @@
+public class Company {
+    public static void main(String[] args) {
+
+        Employee employee1 = new Employee ();
+        employee1.name = "John";
+        employee1.lastname = "Smith";
+        employee1.yearOfBirth = 1978;
+        employee1.gender = "male";
+
+        Employee employee2 = new Employee ();
+        employee2.name = "Xena";
+        employee2.lastname = "Makapaca";
+        employee2.yearOfBirth = 1985;
+        employee2.gender = "female";
+
+        Employee employee3 = new Employee ();
+        employee3.name = "Mick";
+        employee3.lastname = "Simons";
+        employee3.yearOfBirth = 1974;
+        employee3.gender = "male";
+
+    }
+}
---
+++ src/Employee.java
@@ -0,0 +1,6 @@
+public class Employee {
+        String name;
+        String lastname;
+        String gender;
+        int yearOfBirth;
+}

4014eb29ba10041ee443cf2b45bef6f213ba8adf
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2020-06-08 22:45:19 +0200

    feat: printed all objects.

--- src/Company.java
+++ src/Company.java
@@ -12,12 +12,29 @@ public class Company {
         employee2.lastname = "Makapaca";
         employee2.yearOfBirth = 1985;
         employee2.gender = "female";
+        String employee2Info = employee2.gender + ", " + employee2.name + ", " + employee2.lastname + ", " +
+                employee2.yearOfBirth;
+
 
         Employee employee3 = new Employee ();
         employee3.name = "Mick";
         employee3.lastname = "Simons";
         employee3.yearOfBirth = 1974;
         employee3.gender = "male";
+
+
+        System.out.println(employee1.gender);
+        System.out.println(employee1.name);
+        System.out.println(employee1.lastname);
+        System.out.println(employee1.yearOfBirth);
+
+        System.out.println(employee2Info);
+
+        System.out.println(employee3.gender);
+        System.out.println(employee3.yearOfBirth);
+        System.out.println(employee3.lastname);
+        System.out.println(employee3.name);
+
 
     }
 }

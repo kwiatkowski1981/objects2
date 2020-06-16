b876284256074496e379716e2e22fd3cd55b3ba0
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2020-06-17 00:25:09 +0200

    changed structure to objects with constructors and methods

--- src/Company.java
+++ src/Company.java
@@ -1,40 +1,16 @@
 public class Company {
     public static void main(String[] args) {
 
-        Employee employee1 = new Employee ();
-        employee1.name = "John";
-        employee1.lastname = "Smith";
-        employee1.yearOfBirth = 1978;
-        employee1.gender = "male";
-
-        Employee employee2 = new Employee ();
-        employee2.name = "Xena";
-        employee2.lastname = "Makapaca";
-        employee2.yearOfBirth = 1985;
-        employee2.gender = "female";
-        String employee2Info = employee2.gender + ", " + employee2.name + ", " + employee2.lastname + ", " +
-                employee2.yearOfBirth;
 
 
-        Employee employee3 = new Employee ();
-        employee3.name = "Mick";
-        employee3.lastname = "Simons";
-        employee3.yearOfBirth = 1974;
-        employee3.gender = "male";
 
 
-        System.out.println(employee1.gender);
-        System.out.println(employee1.name);
-        System.out.println(employee1.lastname);
-        System.out.println(employee1.yearOfBirth);
+        int maxEmployees = 100;
+        int currentEmployees = 0;
 
-        System.out.println(employee2Info);
+        Employee[] employees = new Employee[maxEmployees];
 
-        System.out.println(employee3.gender);
-        System.out.println(employee3.yearOfBirth);
-        System.out.println(employee3.lastname);
-        System.out.println(employee3.name);
-
-
+        employees[0] = new Employee ("marian", "Durny", "mężczyzna", 1976);
+        employees[0].printInfo();
     }
 }
--- src/Employee.java
+++ src/Employee.java
@@ -1,6 +1,58 @@
 public class Employee {
         String name;
-        String lastname;
+        String lastName;
         String gender;
         int yearOfBirth;
+
+      public  Employee(String name, String lastName, String gender, int yearOfBirth) {
+
+                this.name = name;
+                this.lastName = lastName;
+                this.gender = gender;
+                this.yearOfBirth = yearOfBirth;
+        }
+
+        void printInfo() {
+
+                String info = ("Imię:" + " " + getName()+ "\n" + "Nazwisko:"  + " " + getLastName() + "\n"
+                        + "Płeć:"  + " " + getGender() + "\n" +  "Data urodzenia:"  + " " + getYearOfBirth());
+                System.out.println(info);
+        }
+
+        public String getName() {
+                return name;
+        }
+
+        public void setName(String name) {
+                this.name = name;
+        }
+
+        public String getLastName() {
+                return lastName;
+        }
+
+        public void setLastName(String lastName) {
+                this.lastName = lastName;
+        }
+
+        public String getGender() {
+                return gender;
+        }
+
+        public void setGender(String gender) {
+                this.gender = gender;
+        }
+
+        public int getYearOfBirth() {
+                return yearOfBirth;
+        }
+
+        public void setYearOfBirth(int yearOfBirth) {
+                this.yearOfBirth = yearOfBirth;
+        }
+
+
 }
+
+
+
